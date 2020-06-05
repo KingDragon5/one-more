b69a0d4cbf3e0ac580ad597d55c122f78ad9c468
Ivan Ortega <ivangoku89@gmail.com>
2020-06-05 08:46:10 -0600

    no c

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require ('express');
+
+const hbs = require ('hbs');
+
+const app = express();
+
+app.set('view engine', 'hbs');
+
+hbs.registerPartials(__dirname + '/views/partials');
+
+app.use(express.static(__dirname + '/public'));
+
+//nota rapida
+app.get('/', (req, res)=>{
+    res.render('index');
+});
+
+app.get('/Mrphillip', (req, res)=>{
+    res.render('mistefili');
+});
+
+app.get('/sakimichan', (req, res)=>{
+    res.render('sakimi');
+});
+
+app.get('/piyo', (req, res)=>{
+    res.render('piyo');
+});
+
+app.get('/Mato', (req, res)=>{
+    res.render('Mato');
+});
+
+app.get('/pascurodri', (req, res)=>{
+    res.render('pascurodri');
+});
+
+
+app.listen(3000, ()=>{
+    console.log('escuchando el puerto 3000')
+
+});
